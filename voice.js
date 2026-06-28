66413a34255ee5ac4f3ad3dfa4f6d0782cca55a6
pedroferreira6231 <pedroferreira6231@gmail.com>
2026-06-28 15:55:06 +0100

    Update voice.js

--- voice.js
+++ voice.js
@@ -1,3 +1,16 @@
 let recognition = null;
 let microfoneLigado = false;
 let reiniciarMicrofone = false;
+function iniciarMicrofone() {
+  const SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+
+  if (!SpeechRecognition) {
+    falar("Este navegador não suporta reconhecimento de voz.");
+    return;
+  }
+
+  if (microfoneLigado) {
+    falar("Microfone já está ativo.");
+    return;
+  }
