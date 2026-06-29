6fd0acb23dd35594a0f2f9132d4b5e6511e03039
pedroferreira6231 <pedroferreira6231@gmail.com>
2026-06-29 11:43:24 +0100

    Update maps.js

--- maps.js
+++ maps.js
@@ -11,18 +11,16 @@ estado.destino=destino;
 localStorage.setItem("destinoIntelcar",destino);
 falar("A abrir navegação para "+destino+".");
 setTimeout(()=>abrirGoogleMaps(),700);
-}
-
 function abrirGoogleMaps(){
 const destino=estado.destino||localStorage.getItem("destinoIntelcar");
 if(!destino){
 falar("Define primeiro um destino.");
 alert("Define primeiro um destino.");
 return;
-}const url="https://www.google.com/maps/dir/?api=1&destination="+encodeURIComponent(destino)+"&travelmode=driving";
-window.location.href=url;
-
 }
+
+
+
 
 function mapsComecouAFalar(){estado.mapsFala=true;}
 function mapsParouDeFalar(){estado.mapsFala=false;}
